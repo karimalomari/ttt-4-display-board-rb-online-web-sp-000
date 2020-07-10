a647d49da68151c37a7b02dd71a641b441546724
Karim Alomari <karim.alomari@gmail.com>
2020-07-10 16:38:44 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -149,7 +149,9 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(rows[4]).to eq(" X | X | X ")
     end
 
-
+      # Don't touch the following lines.
+      output = capture_puts{ display_board(board) } if defined?(display_board)
+      rows = output.split("\n")
 
       # Each line that starts with expect represents a row in the ouput.
       # The desired characters a row must include are provided by the String
